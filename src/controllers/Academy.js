3ee0d434a01c9f8be469369e4209b535b892b274
Luiz-2002 <gugacruzbastos@hotmail.com>
2021-04-09 13:35:47 -0300

    implementação de controller academia

--- src/controllers/Academy.js
+++ src/controllers/Academy.js
@@ -1,4 +1,5 @@
 const User_Student = require("../models/UserStudent");
+const AddressAcademy = require("../models/AddressAcademy");
 const Academy = require("../models/Academy");
 
 module.exports = {
@@ -8,7 +9,50 @@ module.exports = {
     find(req, res){},
 
     store(req, res){
+        const {
+            name,
+            cnpj,
+            telefone,
+            email,
+            password,
+            cep,
+            street,
+            state,
+            city,
+        } = req.body;
+        try{
+            const Address_Academy = AddressAcademy.create({
+                cep,
+                street,
+                state,
+                city,
+            });
 
+            const AcademyRegister = Academy.create({
+                name,
+                cnpj,
+                telefone,
+                email,
+                password,
+            });
+            console.log(AcademyRegister);
+
+            Address_Academy.addAcademy(Address_Academy);
+
+            res.status(201).send({
+                AcademyRegister: {
+                    academy_id: AcademyRegister.id,
+                    name: AcademyRegister.name,
+                    cnpj: AcademyRegister.cnpj,
+                    telefone: AcademyRegister.telefone,
+                    email: AcademyRegister.email,
+                    password: AcademyRegister.password,
+                }
+            });
+        }catch (error){
+            console.log(error);
+            res.status(500).send(error);
+        }
     },
 
     update(req, res){},
