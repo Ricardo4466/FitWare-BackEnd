9c4550fca4dbde52a4700d7e09f26f1d297341c2
Luiz-2002 <gugacruzbastos@hotmail.com>
2021-04-19 13:39:26 -0300

    correções

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,12 @@ const express = require("express");
 // Importar o controller
 const UserStudentController =  require("./controllers/User_Students");
 
+const AcademyController = require("./controllers/Academy");
 
 
 const routes = express.Router();
+
+routes.post("/RegisterAcademy", AcademyController.store);
 
 routes.post("/UserAcademy", UserStudentController.store);
 
